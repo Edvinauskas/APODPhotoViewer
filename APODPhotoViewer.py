df58d5f7e156238a87ae3a2dc23ebc46126df724
Edvinas <edvinas@linux-main.(none)>
2012-06-19 17:59:48 +0300

    If the APOD page contains a video, nothing is shown, user recieves message with a link to the page.

--- APODPhotoViewer.py
+++ APODPhotoViewer.py
@@ -12,21 +12,28 @@ class GetImageThread(QThread):
         QThread.__init__(self, parent)
         self.exiting = False
         self.days_to_go_back = days_to_go_back
-
+        
     def run(self):
         page_url = self.get_page_url()
         page_html = self.get_page_html(page_url)
-        image_url, image_name = self.parse_for_image_link_and_title(page_html)
-        image_url = "http://apod.nasa.gov/apod/image/%s" % image_url[0]
-        image_data = self.get_image_from_url(image_url)
-        image_title = QString(image_name[0])
-        final_image = QImage()
-        final_image.loadFromData(image_data)
-        self.emit(SIGNAL("finished(QString, QImage)"),
-            image_title,
-            final_image
-            )
-
+        image_url, image_title = self.parse_for_image_link_and_title(page_html)
+        image_title = image_title[0]
+        image = QImage()
+        if not image_url or not image_title:
+            self.emit(SIGNAL("finished(QString, QString, QImage)"),
+                page_url,
+                image_title,
+                image
+                )
+        else:
+            image_url = "http://apod.nasa.gov/apod/image/%s" % image_url[0]
+            image_data = self.get_image_from_url(image_url)
+            image.loadFromData(image_data)
+            self.emit(SIGNAL("finished(QString, QString, QImage)"),
+                page_url,
+                image_title,
+                image
+                )
 
     def start_thread(self):
         self.start()
@@ -45,10 +52,7 @@ class GetImageThread(QThread):
 
     def parse_for_image_link_and_title(self, html):
         image_url = re.findall('<a href="image/(.*?)"', html)
-        print image_url, " image URL"
-        # image_name = re.findall('<center>\n<b>(.*?)</b> <br>', html)
         image_name = re.findall("<title> APOD: (.*?)\n</title>", html)
-        print html
         return image_url, image_name
 
     def get_image_from_url(self, image_url):
@@ -77,11 +81,11 @@ class APODPhotoViewer(QWidget):
 
     def GUI(self):        
         self.picture_title = QLabel("")
-        self.picture_title.setStyleSheet("QLabel {font-size: 20px;}")
+        self.picture_title.setStyleSheet("QLabel {font-size: 20px; font-weight: bold;}")
         self.picture_title.setFixedHeight(30)
         self.picture_title.setAlignment(Qt.AlignCenter)
         self.display_picture_label = QLabel()
-        self.display_picture_label.setStyleSheet("QLabel {background-color : grey;}")
+        self.display_picture_label.setStyleSheet("QLabel {background-color : grey; font-size: 30px; font-weight: bold;}")
         self.display_picture_label.setAlignment(Qt.AlignCenter)
 
         title_picture_grid = QVBoxLayout()
@@ -108,10 +112,12 @@ class APODPhotoViewer(QWidget):
         
 
     def get_image(self):
+        self.next_button.setEnabled(False)
+        self.prev_button.setEnabled(False)
         self.picture_title.setText("")
         self.get_image_thread = GetImageThread(self.days_to_go_back) 
         self.set_loading_image_text()
-        self.connect(self.get_image_thread, SIGNAL('finished(QString, QImage)'), self.set_image)
+        self.connect(self.get_image_thread, SIGNAL('finished(QString, QString, QImage)'), self.set_image)
         self.get_image_thread.start_thread()
 
     def load_next_image(self):
@@ -133,16 +139,27 @@ class APODPhotoViewer(QWidget):
     def set_loading_image_text(self):
         self.display_picture_label.setText("Loading...")
 
-    def set_image(self, title, image):
-        self.display_picture_label.setText("Done...")
-        self.picture_title.setText(title)
-        self.display_picture_label.setPixmap(
-            QPixmap(image).scaled(
+    def set_image(self, page_link, title, image):
+        image_pixmap = QPixmap(image).scaled(
                 QSize(self.display_picture_label.size()), 
                 Qt.KeepAspectRatio, 
                 Qt.FastTransformation
             )
-        )
+        if image_pixmap.isNull():
+            self.display_picture_label.setText(
+                "<qt>Image not found. It might be a video, <a href='" + str(page_link)  + "'>click here</a> to visit the page.</qt>"
+            )
+            self.picture_title.setText(title)
+            self.connect(self.display_picture_label, SIGNAL("linkActivated(QString)"), self.open_URL)
+        else:
+            self.display_picture_label.setPixmap(image_pixmap)
+            self.picture_title.setText(title)
+
+        self.next_button.setEnabled(True)
+        self.prev_button.setEnabled(True)
+
+    def open_URL(self, URL):
+        QDesktopServices().openUrl(QUrl(URL))
 
 if __name__ == '__main__':
     APODPhotoViewerMain = QApplication(sys.argv)
