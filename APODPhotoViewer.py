946942825f5369c291eb43e64f35a4c9959fa29a
Edvinas <edvinas@linux-main.(none)>
2012-06-19 14:14:36 +0300

    Image is loaded at start up, need to fix start up date, and fix HTML parsing for next-prev dates

--- APODPhotoViewer.py
+++ APODPhotoViewer.py
@@ -4,9 +4,8 @@ import sys
 import urllib2, urllib
 from time import strftime
 import datetime
-import re
+import re, os
 import cStringIO
-import Image
 
 class GetImageThread(QThread):
     def __init__(self, days_to_go_back, parent=None):
@@ -15,25 +14,22 @@ class GetImageThread(QThread):
         self.days_to_go_back = days_to_go_back
 
     def run(self):
-        print "running..."
         page_url = self.get_page_url()
         page_html = self.get_page_html(page_url)
         image_url, image_name = self.parse_for_image_link_and_title(page_html)
         image_url = "http://apod.nasa.gov/apod/image/%s" % image_url[0]
-        image = self.get_image_from_url(image_url)
-        print type(image)
+        image_data = self.get_image_from_url(image_url)
         image_title = QString(image_name[0])
-        final_image = QImage(image)
+        final_image = QImage()
+        final_image.loadFromData(image_data)
         self.sleep(1)
         self.emit(SIGNAL("finished(QString, QImage)"),
             image_title,
             final_image
             )
-        print "Done..."
 
 
     def start_thread(self):
-        print "Starting thread..."
         self.start()
 
     def get_page_url(self):
@@ -41,34 +37,21 @@ class GetImageThread(QThread):
         minus_days = datetime.timedelta(days=self.days_to_go_back)
         image_date = today_date + minus_days
         page_url = "http://apod.nasa.gov/apod/ap%s.html" % image_date.strftime("%y%m%d")
-        # self.image_title_date = "%s" % image_date.strftime("%d/%m/%Y")
-        print page_url, " page url"
         return page_url
 
     def get_page_html(self, page_url):
-        print "Fetching html..."
         open_url = urllib2.urlopen(page_url)
         html = open_url.read()
-        print "Got page html..."
         return html
 
     def parse_for_image_link_and_title(self, html):
-        print "Parsing HTML..."
         image_url = re.findall('<a href="image/(.*?)">', html)
         image_name = re.findall('<center>\n<b>(.*?)</b> <br> \n<b> Image Credit', html)
-        print "Done parsing HTML..."
-        print image_url
-        print image_name
         return image_url, image_name
 
     def get_image_from_url(self, image_url):
-        print image_url
-        import urllib, cStringIO
-        img_file = cStringIO.StringIO(urllib.urlopen(image_url).read())
-        image_file = Image.open(img_file)
-        # image_file = urllib2.urlopen(image_url).read()
-        # print type(image_file)
-        return image_file
+        image_data = urllib.urlopen(image_url).read()
+        return image_data
 
     def __del__(self):
         self.exiting = True
@@ -78,7 +61,7 @@ class APODPhotoViewer(QWidget):
     def __init__(self, win_parrent = None):
         super(APODPhotoViewer, self).__init__()
 
-        self.days_to_go_back = 0
+        self.days_to_go_back = -1
         self.too_far_forward = False
         self.image_title_date = ""
         self.page_url = ""
@@ -117,7 +100,7 @@ class APODPhotoViewer(QWidget):
 
         self.setLayout(self.vbox)
 
-    	self.setFixedSize(1100, 900)
+    	self.setFixedSize(1200, 900)
         self.setWindowTitle('Astronomy Picture of the Day Photo Viewer')
         
 
@@ -133,6 +116,7 @@ class APODPhotoViewer(QWidget):
             self.too_far_forward = True
         else:
             self.days_to_go_back += 1
+            self.get_image()
 
     def load_prev_image(self):
         if self.too_far_forward is True:
@@ -140,20 +124,21 @@ class APODPhotoViewer(QWidget):
             self.too_far_forward = False
         else:
             self.days_to_go_back -= 1
+            self.get_image()
 
     def set_loading_image_text(self):
         self.display_picture_label.setText("Loading...")
 
     def set_image(self, title, image):
-        print type(image), " testing"
         self.display_picture_label.setText("Done...")
         self.picture_title.setText(title)
-        self.main_picture_pixmap = QPixmap.fromImage(image).scaled(
-            QSize(self.display_picture_label.size()), 
-            Qt.KeepAspectRatio, 
-            Qt.FastTransformation
+        self.display_picture_label.setPixmap(
+            QPixmap(image).scaled(
+                QSize(self.display_picture_label.size()), 
+                Qt.KeepAspectRatio, 
+                Qt.FastTransformation
+            )
         )
-        self.display_picture_label.setPixmap(self.main_picture_pixmap)
 
 if __name__ == '__main__':
     APODPhotoViewerMain = QApplication(sys.argv)
